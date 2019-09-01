c9c906e2f7e5885328096ac0078d6b25f55b5196
VictorBramy <53178184+VictorBramy@users.noreply.github.com>
2019-09-01 19:09:45 +0300

    changes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -24,7 +24,7 @@ class Register extends React.Component {
     }
 
     onSubmitClick = () => {
-        fetch('http://localhost:3000/register' , {
+        fetch('https://stark-refuge-28710.herokuapp.com/register' , {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitClick = () => {
-        fetch('http://localhost:3000/signin' , {
+        fetch('https://stark-refuge-28710.herokuapp.com/signin' , {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
